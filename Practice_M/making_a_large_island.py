0b4ebf7e86dfedfb87897a481c9995f5ca127e8f
Soumil Kulkarni <soumilkulkarni@Soumils-MacBook-Pro.local>
2024-03-19 23:23:02 -0700

    Practice Dump from 03/19/2024

---
+++ Practice_M/making_a_large_island.py
@@ -0,0 +1,62 @@
+"""
+Author: Soumil Ramesh Kulkarni
+Date: 03.19.2024
+
+Question:
+You are given an n x n binary matrix grid. You are allowed to change at most one 0 to be 1.
+
+Return the size of the largest island in grid after applying this operation.
+
+An island is a 4-directionally connected group of 1s.
+
+
+
+Example 1:
+
+Input: grid = [[1,0],[0,1]]
+Output: 3
+Explanation: Change one 0 to 1 and connect two 1s, then we get an island with area = 3.
+Example 2:
+
+Input: grid = [[1,1],[1,0]]
+Output: 4
+Explanation: Change the 0 to 1 and make the island bigger, only one island with area = 4.
+Example 3:
+
+Input: grid = [[1,1],[1,1]]
+Output: 4
+Explanation: Can't change any 0 to 1, only one island with area = 4.
+"""
+
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+
+        def neighbors(r, c):
+            for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                if 0 <= nr < N and 0 <= nc < N:
+                    yield nr, nc
+
+        def dfs(r, c, index):
+            ans = 1
+            grid[r][c] = index
+            for nr, nc in neighbors(r, c):
+                if grid[nr][nc] == 1:
+                    ans += dfs(nr, nc, index)
+            return ans
+
+        area = {}
+        index = 2
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c] == 1:
+                    area[index] = dfs(r, c, index)
+                    index += 1
+
+        ans = max(area.values() or [0])
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c] == 0:
+                    seen = {grid[nr][nc] for nr, nc in neighbors(r, c) if grid[nr][nc] > 1}
+                    ans = max(ans, 1 + sum(area[i] for i in seen))
+        return ans
